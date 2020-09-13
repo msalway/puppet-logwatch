e93f37fca47deae1c70baf8cb6bda226f7592a30
Mike Salway <38225053+msalway@users.noreply.github.com>
2020-09-13 16:31:58 +0000

    Test all supported OSs

--- spec/classes/logwatch_spec.rb
+++ spec/classes/logwatch_spec.rb
@@ -1,28 +1,30 @@
 require 'spec_helper'
 
 describe 'logwatch', type: :class do
-  let(:facts) do
-    { osfamily: 'RedHat', operatingsystem: 'Centos' }
-  end
+  on_supported_os.each do |os, os_facts|
+    context "on #{os}" do
+      let(:facts) { os_facts }
 
-  it { is_expected.to contain_class('logwatch::params') }
-  it { is_expected.to contain_class('logwatch::install') }
-  it { is_expected.to contain_class('logwatch::config') }
+      it { is_expected.to contain_class('logwatch::params') }
+      it { is_expected.to contain_class('logwatch::install') }
+      it { is_expected.to contain_class('logwatch::config') }
 
-  it do
-    is_expected.to contain_package('logwatch').with(
-      ensure: 'present',
-      name:   'logwatch',
-    )
-  end
+      it do
+        is_expected.to contain_package('logwatch').with(
+          ensure: 'present',
+          name:   'logwatch',
+        )
+      end
 
-  it do
-    is_expected.to contain_file('logwatch.conf').with(
-      'ensure' => 'file',
-      'path'   => '/etc/logwatch/conf/logwatch.conf',
-      'owner'  => 'root',
-      'group'  => 'root',
-      'mode'   => '0644',
-    )
+      it do
+        is_expected.to contain_file('logwatch.conf').with(
+          'ensure' => 'file',
+          'path'   => '/etc/logwatch/conf/logwatch.conf',
+          'owner'  => 'root',
+          'group'  => 'root',
+          'mode'   => '0644',
+        )
+      end
+    end
   end
 end
